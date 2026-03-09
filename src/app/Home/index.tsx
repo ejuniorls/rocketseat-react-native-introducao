f71a3e5af84642253a9a7a799719094a320e7c07
Edmir Lôbo <ejuniorls@outlook.com>
2026-03-09 16:06:18 -0300

    componente button

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,10 +1,14 @@
 import {View, Text, StyleSheet, Image} from "react-native";
+
+import {Button} from "@/components/Button";
+
 import {styles} from "./styles";
 
 export function Home() {
     return (
         <View style={styles.container}>
             <Image source={require('@/assets/logo.png')} style={styles.logo}/>
+            <Button></Button>
         </View>
     );
 }
