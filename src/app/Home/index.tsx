938baaf5df9fa8d2e4678b2ed14befa9a76f5bf6
Edmir Lôbo <ejuniorls@outlook.com>
2026-03-09 16:51:28 -0300

    utilizando propriedades no componente

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,5 +1,6 @@
 import {View, Text, StyleSheet, Image} from "react-native";
 
+// @ts-ignore
 import {Button} from "@/components/Button";
 
 import {styles} from "./styles";
@@ -8,7 +9,8 @@ export function Home() {
     return (
         <View style={styles.container}>
             <Image source={require('@/assets/logo.png')} style={styles.logo}/>
-            <Button></Button>
+
+            <Button title="Opacity 0.1"/>
         </View>
     );
 }
