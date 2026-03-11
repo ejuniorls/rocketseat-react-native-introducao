046220db1ccedcf34b33c0de5bbdef57a097cb32
Edmir Lôbo <ejuniorls@outlook.com>
2026-03-11 15:25:11 -0300

    icones

--- src/components/Filter/index.tsx
+++ src/components/Filter/index.tsx
@@ -1,4 +1,6 @@
 import {Text, TouchableOpacity, TouchableOpacityProps} from "react-native";
+import {CircleCheck} from "lucide-react-native";
+
 import {FilterStatus} from "@/types/FilterStatus";
 
 import {styles} from "./styles";
@@ -15,8 +17,9 @@ export function Filter({status, isActive, ...rest}: Props) {
             activeOpacity={0.8}
             {...rest}
         >
+            <CircleCheck size={18} color={"#000"} />
             <Text style={styles.title}>
-                {status === FilterStatus.DONE ? "Comprados" : "Pendentes"}
+                {status === FilterStatus.DONE ? "Compradossss" : "Pendentes"}
             </Text>
         </TouchableOpacity>
     );
